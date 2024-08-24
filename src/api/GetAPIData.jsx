76c17f91b4907ac554d086e9c92a9bb86ea5568d
thapa technical <vbthapa55@gmail.com>
2024-08-24 10:47:50 +0530

    env files added

--- src/api/GetAPIData.jsx
+++ src/api/GetAPIData.jsx
@@ -1,7 +1,9 @@
 export const getMoviesData = async () => {
   try {
     const response = await fetch(
-      "http://www.omdbapi.com/?i=tt3896198&apikey=1c12799f&s=titanic&page=1"
+      `http://www.omdbapi.com/?i=tt3896198&apikey=${
+        import.meta.env.VITE_API_KEY
+      }&s=titanic&page=1`
     );
     const data = response.json();
     return data;
